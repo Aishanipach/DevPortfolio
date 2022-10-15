3d9070db446c74b85ac7ae0abec32b0483eb0ff1
Aishanipach <aishani.pachauri@gmail.com>
2022-10-15 18:08:53 +0530

    second  draft

--- src/components/CardGrids.js
+++ src/components/CardGrids.js
@@ -1,32 +1,35 @@
 import React from 'react'
 import "./CardGrids.css"
 import proj1 from "./images/proj1.svg"
+import proj2 from "./images/proj2.svg"
+import proj3 from "./images/proj3.svg"
+import proj4 from "./images/proj4.svg"
 function CardGrids() {
   return (
     <div>
       <div class="wrapper">
         <div class="card" id="newr"><img src={proj1} />
           <div class="info">
-            <h1>Mountain</h1>
-            <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
             <button>Read More</button>
           </div>
         </div>
-        <div class="card" id="newr"><img src="https://images.unsplash.com/photo-1425342605259-25d80e320565?auto=format&amp;fit=crop&amp;w=750&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+        <div class="card" id="newr"><img src={proj3} />
           <div class="info">
             <h1>Road</h1>
             <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
             <button>Read More</button>
           </div>
         </div>
-        <div class="card" id="newr"><img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?auto=format&amp;fit=crop&amp;w=311&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+        <div class="card" id="newr"><img src={proj2} />
           <div class="info">
             <h1>Protester</h1>
             <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
             <button>Read More</button>
           </div>
         </div>
-        <div class="card" id="newr"><img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?auto=format&amp;fit=crop&amp;w=311&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
+        <div class="card" id="newr"><img src={proj4} />
           <div class="info">
             <h1>Protester</h1>
             <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
