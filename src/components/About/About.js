3a68a350ea278ed7029251a545fbaf22d6a34185
Aishanipach <aishani.pachauri@gmail.com>
2022-10-15 21:27:02 +0530

    third  draft

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,8 +7,9 @@ function About() {
     return (
         <Container style={{ marginTop: "35vh", textAlign: "left" }} >
             <h1 className='linear-wipe'> Always Curious.</h1>
-            <div style={{ width: "23vw", marginTop: "1.4rem" }}>
-                <Image src={icons} fluid></Image>
+            <div className="d-flex row " style={{ width: "23vw", marginTop: "1.4rem", color: "grey", flexWrap: "nowrap" }}>
+                <Image src={icons} style={{ width: "350px" }} fluid></Image>
+
             </div>
             <h5 style={{ fontWeight: "300", marginTop: "2.8rem", fontSize: "16px" }}>I love building web experiences and have a knack to solve problems. I have been coding in CPP since class 11th and<br /> I think what made me stick around to pursue BTech in CSE would be the fact that there is logic behind everything.</h5>
         </Container>
--- src/components/CardGrids.js
+++ src/components/CardGrids.js
@@ -17,22 +17,36 @@ function CardGrids() {
         </div>
         <div class="card" id="newr"><img src={proj3} />
           <div class="info">
-            <h1>Road</h1>
-            <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
             <button>Read More</button>
           </div>
         </div>
         <div class="card" id="newr"><img src={proj2} />
           <div class="info">
-            <h1>Protester</h1>
-            <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
             <button>Read More</button>
           </div>
         </div>
         <div class="card" id="newr"><img src={proj4} />
           <div class="info">
-            <h1>Protester</h1>
-            <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
+            <button>Read More</button>
+          </div>
+        </div>
+        <div class="card" id="newr"><img src={proj2} />
+          <div class="info">
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
+            <button>Read More</button>
+          </div>
+        </div>
+        <div class="card" id="newr"><img src={proj4} />
+          <div class="info">
+            <h5>OMDB Web App</h5>
+            <p style={{ fontWeight: "300" }}>Movie directory with Firebase authentication, playlist keeper while using OMDB API</p>
             <button>Read More</button>
           </div>
         </div>
